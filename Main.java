00734eca679dcf3fbb6196b39677fd9c1c3769c0
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-07-30 18:43:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args){
+		
+		int age = 25;
+		System.out.println("Age you Provided " +age);
+		if (age > 18){
+			System.out.println("Adult");
+		}
+		else{
+			System.out.println("Child");
+		}
+		
+	}
+}
